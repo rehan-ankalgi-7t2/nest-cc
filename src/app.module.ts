a519657c5375bc751ac3d2a06d898ec5bc9ca710
rehan-ankalgi-7t2 <rehanankalgi7t2@gmail.com>
2024-09-15 20:25:44 +0530

    mongodb connection solved: wrong password

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,6 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
-import { databaseProviders } from './database/database.providers';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
